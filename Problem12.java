d1f72890845728b988679e4af0aeb8a0e7a2ccf4
RefinedStone <chem.en.9273@gmail.com>
2022-10-08 01:36:09 +0900

    알고리즘 11~13번 제출합니다

--- Problem12.java
+++ Problem12.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class Problem12 {
+import java.util.Arrays;
+
+public class Problem12 {
+    public static void main(String[] args) {
+        //최댓값 만들기 (1)
+        //https://school.programmers.co.kr/learn/courses/30/lessons/120847
+        int[] numbers= new int[]{0, 31, 24, 10, 1, 9};
+
+        Arrays.sort(numbers);
+
+        int answer = numbers[numbers.length-1]*numbers[numbers.length-2];
+
+        //return answer;
+    }
 }
--- Problem13.java
+++ Problem13.java
@@ -1,2 +1,5 @@
-package PACKAGE_NAME;public class Problem13 {
+public class Problem13 {
+    public static void main(String[] args) {
+        
+    }
 }
